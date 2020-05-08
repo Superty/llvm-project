941cd6501010f60ec02783808aea2a951e2d9f3f
Arjun P <arjunpitchanathan@gmail.com>
2020-05-08 16:44:51 +0530

    [FlatAffineStructures] add tests for findSample

--- mlir/unittests/Analysis/AffineStructuresTest.cpp
+++ mlir/unittests/Analysis/AffineStructuresTest.cpp
@@ -64,4 +64,47 @@ TEST(FlatAffineConstraintsTest, RemoveRedundantConstraintsEqualities) {
   EXPECT_EQ(fac.getConstantUpperBound(2).getValueOr(-100), 0);
 }
 
+int64_t valueAt(ArrayRef<int64_t> expr, const std::vector<int64_t> &point) {
+  int64_t value = expr.back();
+  assert(expr.size() == 1 + point.size());
+  for (unsigned i = 0; i < point.size(); ++i)
+    value += expr[i] * point[i];
+  return value;
+}
+
+void checkSample(bool hasValue, const FlatAffineConstraints &fac) {
+  auto maybeSample = fac.findSample();
+  if (!hasValue)
+    EXPECT_FALSE(maybeSample.hasValue());
+  else {
+    ASSERT_TRUE(maybeSample.hasValue());
+    for (unsigned i = 0; i < fac.getNumEqualities(); ++i)
+      EXPECT_EQ(valueAt(fac.getEquality(i), *maybeSample), 0);
+    for (unsigned i = 0; i < fac.getNumInequalities(); ++i)
+      EXPECT_GE(valueAt(fac.getInequality(i), *maybeSample), 0);
+  }
+}
+
+FlatAffineConstraints makeFACFromConstraints(unsigned dims, std::vector<std::vector<int64_t>> ineqs, std::vector<std::vector<int64_t>> eqs) {
+  FlatAffineConstraints fac(ineqs.size(), eqs.size(), dims + 1, dims);
+  for (const auto &eq : eqs)
+    fac.addEquality(eq);
+  for (const auto &ineq : ineqs)
+    fac.addInequality(ineq);
+  return fac;
+}
+
+TEST(FlatAffineConstraintsTest, FindSampleTest) {
+  // bounded sets, only inequalities
+  checkSample(false, makeFACFromConstraints(1, {{5, -1}, {-5, 4}}, {}));
+  checkSample(true, makeFACFromConstraints(1, {{5, -1}, {-5, 9}}, {}));
+
+  // bounded sets with equalities
+  checkSample(true, makeFACFromConstraints(2, {{1, 0, -8}, {0, -1, 40}}, {{1, -1, 0}}));
+  // checkSample(true, makeFACFromConstraints(3, {{-1, 0, 0, 10}, {0, -1, 0, 10}, {0, 0, 1, -10}}, {{1, 2, -3, 0}}));
+  // checkSample(false, makeFACFromConstraints(3, {{-1, 0, 0, 10}, {0, -1, 0, 10}, {0, 0, 1, -11}}, {{1, 2, -3, 0}}));
+  // checkSample(true, makeFACFromConstraints(2, {{0, 1, 0}, {0, -1, 3}}, {{4, 1, -7}}));
+  // checkSample(false, makeFACFromConstraints(2, {}, {{4, 1, -7}, {0, 1, 0}}));
+}
+
 } // namespace mlir
