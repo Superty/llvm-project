50d5b3a4bfc0d4b24cb7f412da2b45dc2bf7aa92
Arjun P <arjunpitchanathan@gmail.com>
2020-05-12 16:57:48 +0530

    [FlatAffineStructures] Write explanatory comments for tests for findSample

--- mlir/unittests/Analysis/AffineStructuresTest.cpp
+++ mlir/unittests/Analysis/AffineStructuresTest.cpp
@@ -96,15 +96,35 @@ FlatAffineConstraints makeFACFromConstraints(unsigned dims, std::vector<std::vec
 
 TEST(FlatAffineConstraintsTest, FindSampleTest) {
   // bounded sets, only inequalities
+
+  // 1 <= 5x and 5x <= 4 (no solution)
   checkSample(false, makeFACFromConstraints(1, {{5, -1}, {-5, 4}}, {}));
+
+  // 1 <= 5x and 5x <= 9 (solution: x = 1)
   checkSample(true, makeFACFromConstraints(1, {{5, -1}, {-5, 9}}, {}));
 
   // bounded sets with equalities
+
+  // x >= 8 and 40 >= y and x = y
   checkSample(true, makeFACFromConstraints(2, {{1, 0, -8}, {0, -1, 40}}, {{1, -1, 0}}));
-  // checkSample(true, makeFACFromConstraints(3, {{-1, 0, 0, 10}, {0, -1, 0, 10}, {0, 0, 1, -10}}, {{1, 2, -3, 0}}));
-  // checkSample(false, makeFACFromConstraints(3, {{-1, 0, 0, 10}, {0, -1, 0, 10}, {0, 0, 1, -11}}, {{1, 2, -3, 0}}));
-  // checkSample(true, makeFACFromConstraints(2, {{0, 1, 0}, {0, -1, 3}}, {{4, 1, -7}}));
-  // checkSample(false, makeFACFromConstraints(2, {}, {{4, 1, -7}, {0, 1, 0}}));
+
+  // x <= 10 and y <= 10 and 10 <= z and x + 2y = 3z
+  // solution: x = y = z = 10.
+  checkSample(true, makeFACFromConstraints(3, {{-1, 0, 0, 10}, {0, -1, 0, 10}, {0, 0, 1, -10}}, {{1, 2, -3, 0}}));
+
+  // x <= 10 and y <= 10 and 11 <= z and x + 2y = 3z
+  // This implies x + 2y >= 33 and x + 2y <= 30, which has no solution.
+  checkSample(false, makeFACFromConstraints(3, {{-1, 0, 0, 10}, {0, -1, 0, 10}, {0, 0, 1, -11}}, {{1, 2, -3, 0}}));
+
+  // 0 <= r and r <= 3 and 4q + r = 7
+  // Solution: q = 1, r = 3
+  checkSample(true, makeFACFromConstraints(2, {{0, 1, 0}, {0, -1, 3}}, {{4, 1, -7}}));
+
+  // 4q + r = 7 and r = 0
+  // Solution: q = 1, r = 3
+  checkSample(false, makeFACFromConstraints(2, {}, {{4, 1, -7}, {0, 1, 0}}));
+
+  
 }
 
 } // namespace mlir
