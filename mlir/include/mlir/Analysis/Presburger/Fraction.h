c8dfda629a4c4de7c7db24c5642b3101f0d009c0
Arjun P <arjunpitchanathan@gmail.com>
2020-05-19 15:43:43 +0530

    [Fraction] add file comment

--- mlir/include/mlir/Analysis/Presburger/Fraction.h
+++ mlir/include/mlir/Analysis/Presburger/Fraction.h
@@ -1,3 +1,16 @@
+//===- Simplex.h - MLIR Fraction Class --------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This is a simple class to represent fractions. It supports multiplication,
+// comparison, floor, and ceiling operations.
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef MLIR_ANALYSIS_PRESBURGER_FRACTION_H
 #define MLIR_ANALYSIS_PRESBURGER_FRACTION_H
 
