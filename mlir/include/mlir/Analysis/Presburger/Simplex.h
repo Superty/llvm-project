0eae2cccee42c324d3d60492e93af9ce607dbe0b
webmiche <webmiche@student.ethz.ch>
2020-06-15 10:06:19 +0200

    [Refactoring] implemented Feedback Pt.1

--- mlir/include/mlir/Analysis/Presburger/Simplex.h
+++ mlir/include/mlir/Analysis/Presburger/Simplex.h
@@ -373,7 +373,7 @@ private:
   ///
   /// \returns the separation type, IneqType::SEPARATE, IneqType::ADJ_EQ, or
   /// IneqType::ADJ_INEQ.
-  IneqType separationType(size_t row);
+  IneqType separationType(unsigned row);
 
   /// Checks that \p unknown is neither a redundant row or a dead column
   ///
