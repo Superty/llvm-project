9dc6dc1610abe0cd4b20b33d640d6f81e4239d2c
Arjun P <arjunpitchanathan@gmail.com>
2020-07-24 15:36:09 +0530

    [LinearTransform] split postMultiplyBasicSet(FlatAffineConstraints) into a separate file to avoid dependency cycle

--- mlir/lib/Analysis/Presburger/LinearTransform.cpp
+++ mlir/lib/Analysis/Presburger/LinearTransform.cpp
@@ -5,6 +5,7 @@
 #include "mlir/Analysis/Presburger/LinearTransform.h"
 
 namespace mlir {
+
 LinearTransform::LinearTransform(MatrixType oMatrix)
     : matrix(std::move(oMatrix)) {}
 
@@ -112,34 +113,6 @@ LinearTransform::preMultiplyColumn(ArrayRef<int64_t> col) {
       elem += matrix(row, i) * col[i];
     result.push_back(elem);
   }
-  return result;
-}
-
-FlatAffineConstraints
-LinearTransform::postMultiplyBasicSet(const FlatAffineConstraints &bs) {
-  FlatAffineConstraints result(bs.getNumDimIds());
-
-  for (unsigned i = 0; i < bs.getNumEqualities(); ++i) {
-    ArrayRef<int64_t> eq = bs.getEquality(i);
-
-    int64_t c = eq.back();
-
-    SmallVector<int64_t, 8> newEq = postMultiplyRow(eq.drop_back());
-    newEq.push_back(c);
-    result.addEquality(newEq);
-  }
-
-  for (unsigned i = 0; i < bs.getNumInequalities(); ++i) {
-    ArrayRef<int64_t> ineq = bs.getInequality(i);
-
-    int64_t c = ineq.back();
-
-    SmallVector<int64_t, 8> newIneq = postMultiplyRow(ineq.drop_back());
-    newIneq.push_back(c);
-    result.addInequality(newIneq);
-  }
-
-  // bs.simplify(); // isl does this here
   return result;
 }
 
