2ff844e67a9b97f6a558323fc1972f3b3bdfd538
Arjun P <arjunpitchanathan@gmail.com>
2020-09-04 03:51:53 +0530

    [SetTest] fix header comment

--- mlir/unittests/Analysis/Presburger/SetTest.cpp
+++ mlir/unittests/Analysis/Presburger/SetTest.cpp
@@ -1,4 +1,4 @@
-//===- SimplexTest.cpp - Tests for PresburgerSet---------------------------===//
+//===- SetTest.cpp - Tests for PresburgerSet-------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
