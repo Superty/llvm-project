2bdd17059b9b53a505ef7f804c6ce899d71fb259
Michel Weber <michel.weber@inf.ethz.ch>
2021-09-28 11:51:24 +0200

    update FACUtils license

--- mlir/unittests/Analysis/FACUtils.h
+++ mlir/unittests/Analysis/FACUtils.h
@@ -1,4 +1,4 @@
-//===- utils.h - utility functions for tests ------------------------------===//
+//===- FACUtils.h - utility functions for testing -------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
