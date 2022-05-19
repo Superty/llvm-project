d7efa169722e6bd7da17e870fa6c82523c7a490b
Arjun P <arjunpitchanathan@gmail.com>
2022-05-19 12:38:18 +0100

    matrix

--- mlir/benchmarks/mpint.bench.cpp
+++ mlir/benchmarks/mpint.bench.cpp
@@ -6,7 +6,6 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "mlir/Analysis/Presburger/MPInt.h"
 #include "mlir/Analysis/Presburger/Simplex.h"
 #include "llvm/ADT/ArrayRef.h"
 #include <cstdint>
@@ -40,34 +39,26 @@ static void mulI64(benchmark::State &state) {
 }
 BENCHMARK(mulI64);
 
-static void mulMpint(benchmark::State &state) {
-  MPInt a[N];
-  MPInt b[N];
-  MPInt c[N];
-  for (int i = 0; i < N; i++) {
-	a[i] = i;
-	b[i] = i;
-  }
-
-  for (auto _ : state)
-  // #pragma nounroll
- 	for (int i = 0; i < N; i+=16) {
-	    c[i] = a[i] * b[i];
+static void matrix(benchmark::State &state) {
+  for (auto _ : state) {
+    Matrix mat(2, 2);
+    mat.fillRow(0, 1);
+    mat.fillRow(0, 2);
+    mat.addToRow(0, 1, 1);
+    benchmark::DoNotOptimize(matrix);
 	}
-
-  benchmark::DoNotOptimize(c[42]);
 }
-BENCHMARK(mulMpint);
+BENCHMARK(matrix);
 
 static void simplex(benchmark::State &state) {
-  auto ineq1 = getMPIntVec({1, -1});
-  auto ineq2 = getMPIntVec({-1, 0});
+  llvm::SmallVector<int64_t, 8> ineq1 = {1, -1};
+  llvm::SmallVector<int64_t, 8> ineq2 = {-1, 0};
   for (auto _ : state) {
     Simplex simplex(1);
     simplex.addInequality(ineq1);
     simplex.addInequality(ineq2);
+    benchmark::DoNotOptimize(matrix);
 	}
-  benchmark::DoNotOptimize(simplex);
 }
 BENCHMARK(simplex);
 
