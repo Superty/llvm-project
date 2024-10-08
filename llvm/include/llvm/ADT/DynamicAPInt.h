09bbf50064ec62dc7508458377eff4bef2a35146
Arjun P <arjunpitchanathan@gmail.com>
2024-10-08 16:11:24 +0100

    try build faster

--- llvm/include/llvm/ADT/DynamicAPInt.h
+++ llvm/include/llvm/ADT/DynamicAPInt.h
@@ -17,7 +17,10 @@
 #define LLVM_ADT_DYNAMICAPINT_H
 
 #include "llvm/ADT/SlowDynamicAPInt.h"
+#include "llvm/ADT/Hashing.h"
+#include "llvm/Support/Debug.h"
 #include "llvm/Support/MathExtras.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/raw_ostream.h"
 #include <numeric>
 
@@ -637,6 +640,29 @@ LLVM_ATTRIBUTE_ALWAYS_INLINE bool operator>=(int64_t A, const DynamicAPInt &B) {
     return A >= B.getSmall();
   return A >= B.getLarge();
 }
+
+inline hash_code hash_value(const DynamicAPInt &X) {
+  if (X.isSmall())
+    return llvm::hash_value(X.getSmall());
+  return detail::hash_value(X.getLarge());
+}
+
+inline void DynamicAPInt::static_assert_layout() {
+  constexpr size_t ValLargeOffset =
+      offsetof(DynamicAPInt, ValLarge.Val.BitWidth);
+  constexpr size_t ValSmallOffset = offsetof(DynamicAPInt, ValSmall);
+  constexpr size_t ValSmallSize = sizeof(ValSmall);
+  static_assert(ValLargeOffset >= ValSmallOffset + ValSmallSize);
+}
+
+inline raw_ostream &DynamicAPInt::print(raw_ostream &OS) const {
+  if (isSmall())
+    return OS << ValSmall;
+  return OS << ValLarge;
+}
+
+inline void DynamicAPInt::dump() const { print(dbgs()); }
+
 } // namespace llvm
 
 #endif // LLVM_ADT_DYNAMICAPINT_H
